b939327388fc16aff4c6fffa3c2387ebac3db4e7
glasdou.dev <dr.glasdou@gmail.com>
2024-10-15 08:26:37 -0600

    Refactor BaseCard and ButtonsCard components to improve code structure and maintainability

--- src/modules/shared/components/BaseCard.vue
+++ src/modules/shared/components/BaseCard.vue
@@ -10,19 +10,19 @@ withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-  <BlockUI class="w-full" :blocked="loading">
-    <Card
-      :class="[
-        'dark:bg-dark-950 dark:border-gray-700 border',
-        { ' border-primary-400': !deleted },
-        { 'border-red-500 dark:border-red-600': deleted }
-      ]"
-    >
-      <template #content>
+  <Card
+    :class="[
+      'w-full dark:bg-dark-950 dark:border-gray-700 border',
+      { ' border-primary-400': !deleted },
+      { 'border-red-500 dark:border-red-600': deleted }
+    ]"
+  >
+    <template #content>
+      <BlockUI :blocked="loading">
         <slot />
-      </template>
-    </Card>
-  </BlockUI>
+      </BlockUI>
+    </template>
+  </Card>
 </template>
 
 <style scoped></style>
